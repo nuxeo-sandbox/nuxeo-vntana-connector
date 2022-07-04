7c6107a633f080466c0bab3674533cf8828e348f
michael <mvachette@nuxeo.com>
2022-07-04 15:02:40 +0900

    Add 3D model Annotations

---
+++ nuxeo-vntana-connector-annotation/src/index.js
@@ -0,0 +1,244 @@
+import './styles.css';
+import Nuxeo from 'nuxeo';
+
+
+const nuxeo = new Nuxeo({});
+
+let nxUser;
+let nxDoc;
+let nxAnnotations;
+let nxComments;
+let vnAnnotations;
+let docId;
+let blobUrl;
+let thumbnailUrl;
+
+window.addEventListener("load", function () {
+    const params = new URLSearchParams(window.location.search);
+    docId = params.get('docId');
+    blobUrl = params.get('blobUrl');
+    thumbnailUrl = params.get('thumbnailUrl');
+    getContext().then(() => {
+        setupViewer();
+    });
+});
+
+function getContext() {
+    //get current user
+    return nuxeo.request('me').get().then(user => {
+        nxUser = user;
+        //get current document
+        return nuxeo.repository().enricher('document','thumbnail').fetch(docId, {
+            schemas: ['dublincore', 'file']
+        });
+    }).then(doc => {
+        nxDoc = doc;
+        //set glb blob url
+        if (!blobUrl) {
+            blobUrl = nxDoc.get('file:content').data;
+        }
+        //set thumbnail url
+        if (!thumbnailUrl) {
+            thumbnailUrl = nxDoc.contextParameters.thumbnail.url;
+        }
+
+        //get annotations
+        return nuxeo.request(`id/${docId}/@annotation`).get({
+            queryParams: {
+                xpath: "file:content"
+            }
+        });
+    }).then(annotations => {
+        nxAnnotations = annotations;
+        //get comments
+        return nuxeo.request(`id/${nxDoc.uid}/@annotation/comments`).post({
+            body: annotations.entries.map((annotation) => {return annotation.id})
+        });
+    }).then(comments => {
+        nxComments = comments;
+        vnAnnotations = nx2vntanaAnnotations(nxAnnotations);
+        return Promise.resolve();
+    }).catch(function (error) {
+        throw error;
+    });
+}
+
+function setupViewer() {
+
+    let viewerEl = document.querySelector('#viewer');
+
+    window.vnvieweditor.createViewerEditor(viewerEl, {
+        fileGLTF: blobUrl,
+        thumbnailUrl: thumbnailUrl,
+        panelOrder: ["annotation", "settings"],
+        hideSaveBtn: true,
+        currentUserId: nxUser.id,
+        usersList: [
+            {
+                "userId": nxUser.id,
+                "userName": `${nxUser.properties.firstName} ${nxUser.properties.lastName}`
+            }
+        ],
+        getAnnotations: () => {return vnAnnotations;},
+        createAnnotation: createAnnotation,
+        updateAnnotation: updateAnnotation,
+        deleteAnnotation: deleteAnnotation,
+        resolveAnnotation: resolveAnnotation,
+        getAnnotationComments: getAnnotationComments,
+        createComment: createComment,
+        updateComment: updateComment,
+        deleteComment: deleteComment,
+        onSuccess: () => {
+            let menuElement = document.querySelector('#ViewerEditorBtn');
+            menuElement.click();
+
+            let panels = document.querySelectorAll('.ViewerEditorNavigation div');
+            panels[1].click();
+
+            let navigationElement = document.querySelector('.ViewerEditorNavigation');
+            if (navigationElement) {
+                navigationElement.style.display = "none";
+            }
+        }
+    });
+}
+
+function nx2vntanaAnnotations(nxAnnotations) {
+    let vnAnnotations = nxAnnotations.entries.map((entry,index) => {
+        let vnAnnotation = JSON.parse(entry.entity);
+        return {
+            userId: entry.author,
+            dimensions: vnAnnotation.dimensions,
+            resolved: vnAnnotation.resolved,
+            number: index+1,
+            text: entry.text,
+            uuid: entry.id,
+            created: entry.creationDate.slice(0, -1),
+            updated: entry.modificationDate.slice(0, -1),
+            commentsCount: getAnnotationComments({entityUuid:entry.id}).response.comments.total
+        }
+    });
+    return {
+        response: {
+            annotations: {
+                grid: vnAnnotations,
+                totalCount: vnAnnotations.length,
+                nextNumber: vnAnnotations.length + 1
+            }
+        }
+    }
+}
+
+function createAnnotation(requestData) {
+    return nuxeo.request(`id/${nxDoc.uid}/@annotation`).post({
+        body: {
+            "entity-type": "annotation",
+            xpath: "file:content",
+            author: nxUser.id,
+            text: requestData.text,
+            origin: "vntanaViewer",
+            entity: JSON.stringify(requestData)
+        }
+    }).then(annotation => {
+        return {
+            userId: annotation.author,
+            dimensions: JSON.parse(annotation.entity).dimensions,
+            number: requestData.number,
+            text: annotation.text,
+            uuid: annotation.id
+        }
+    });
+}
+
+function updateAnnotation(requestData) {
+    return nuxeo.request(`id/${nxDoc.uid}/@annotation/${requestData.uuid}`).put({
+        body: {
+            "entity-type": "annotation",
+            xpath: "file:content",
+            text: requestData.text,
+            origin: "vntanaViewer",
+            entity: JSON.stringify(requestData)
+        }
+    }).then(annotation => {
+        return {
+            userId: annotation.author,
+            dimensions: JSON.parse(annotation.entity).dimensions,
+            number: requestData.number,
+            text: annotation.text,
+            uuid: annotation.id
+        }
+    });
+}
+
+function resolveAnnotation(requestData) {
+    return nuxeo.request(`id/${nxDoc.uid}/@annotation/${requestData.uuid}`).get().then(annotation => {
+        let entity = JSON.parse(annotation.entity);
+        entity.resolved = requestData.resolved;
+        return nuxeo.request(`id/${nxDoc.uid}/@annotation/${requestData.uuid}`).put({
+            body: {
+                "entity-type": "annotation",
+                xpath: "file:content",
+                entity: JSON.stringify(entity)
+            }
+        })
+    });
+}
+
+function deleteAnnotation(annotation) {
+    return nuxeo.request(`id/${nxDoc.uid}/@annotation/${annotation.uuid}`).delete();
+}
+
+function getAnnotationComments(annotation) {
+    let vnComments = nxComments.entries.flatMap(entry => {
+        if (entry.parentId === annotation.entityUuid) {
+            return [{
+                userId: entry.author,
+                "entityType": "ANNOTATION",
+                message: entry.text,
+                uuid: entry.id,
+                entityUuid: annotation.entityUuid,
+                created: entry.creationDate,
+                updated: entry.modificationDate,
+            }];
+        } else {
+            return [];
+        }
+    });
+    return {
+        response: {
+            comments: {
+                grid: vnComments,
+                total: vnComments.length,
+            }
+        }
+    }
+}
+
+function createComment(requestData) {
+    nuxeo.request(`id/${requestData.entityUuid}/@comment`).post({
+        body: {
+            "entity-type": "comment",
+            text: requestData.message,
+            origin: "vntanaViewer"
+        }
+    });
+    return {
+        entityUuid: requestData.entityUuid,
+        uuid: 'tmp1'
+    };
+}
+
+function updateComment(requestData) {
+    let uuid = requestData.uuid;
+    return nuxeo.request(`id/${requestData.entityUuid}/@comment/${uuid}`).put({
+        body: {
+            "entity-type": "comment",
+            text: requestData.message
+        }
+    });
+}
+
+function deleteComment(uuid) {
+    return nuxeo.request(`id/${uuid}`).delete();
+}
+
---
+++ nuxeo-vntana-connector-annotation/webpack.config.js
@@ -0,0 +1,66 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+const dotenv = require('dotenv');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const stylesHandler = 'style-loader';
+
+dotenv.config();
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+        port: 8000,
+        open: ['/index.html?docId=a2c6d2fd-1b68-48e0-8301-3c722908e197'],
+        proxy: {
+            '/nuxeo': {
+              target: process.env.DEV_SERVER_URL,
+              logLevel: "debug",
+              ws: true,
+              changeOrigin: true
+            }
+        }
+    },
+    plugins: [
+        new ESLintPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            title:"3D Annotation"
+        }),
+
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [stylesHandler,'css-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+};
+
+module.exports = () => {   
+    if (isProduction) {
+        config.mode = 'production';
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
